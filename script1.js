64fe04bd4faa8fc0395f83bf25420436e293a032
Tim Sengsouvanh <timsengsouvanh@gmail.com>
2021-01-19 20:19:56 +0900

    finished dom

--- script1.js
+++ script1.js
@@ -6,84 +6,58 @@ return choice[Math.floor(Math.random()*choice.length)];
 }
 
 const choice = ['rock', 'paper', 'scissors'];
+//////////////////////////////////////////////////////
 
-//setting the computerSelection as the result of function
+let playerscore = 0;
+let computerscore = 0;
 
+//dom variables
 
-// console.log(computerSelection)
-
-//player selection
-
-
-// function playerPlay(){
-//     const userInput = prompt('rock, paper or scissors?')
-//     return userInput.toLowerCase()
-// }
-const rock = document.querySelector('#rock')
-const paper = document.querySelector('#paper')
-const scissors = document.querySelector('#scissors')
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
 const notification = document.createElement('div');
+const defaultscore = document.createElement('div');
+const defaultscore1 = document.createElement('div');
+const selectionsarea = document.querySelector('.selectionsarea')
 
+//notification dom
+const youwinlose = document.querySelector('.youwinlose')
 
+//outcome functions
 function youwin(){
 
 notification.classList.add('notification')
 notification.textContent = 'You Win!';
-scorearea.appendChild(notification)}
+youwinlose.appendChild(notification);
+playerscore++;
+}
 
 function youlose(){
 
 notification.classList.add('notification')
 notification.textContent = 'You Lose :(';
-scorearea.appendChild(notification)}
+youwinlose.appendChild(notification)
+computerscore++;
+}
 
 function youdraw(){
 
 notification.classList.add('notification')
 notification.textContent = 'You Draw...';
-scorearea.appendChild(notification)}
+youwinlose.appendChild(notification)}
 
-rock.addEventListener('click', () => {
-      playerSelection = 'rock'
-      notification.remove()
-      playRound()
-    })
-paper.addEventListener('click', () => {
-      playerSelection = 'paper'
-      notification.remove()
-      playRound()
-    })
-scissors.addEventListener('click', () => {
-      playerSelection = 'scissors'
-      notification.remove()
-      playRound()
-    })
-
-//notification dom
-const scorearea = document.querySelector('#scorearea')
-
-
-
-
-
-//setting the playerSelection as the result of function
-// const playerSelection = playerPlay()
-
-// console.log(playerSelection)
 
 //rules for the game
 function playRound() {
     const computerSelection = computerPlay()
     console.log(computerSelection)
-  
-    // const playerSelection = playerPlay()
-    // console.log(playerSelection)
 
     if (playerSelection === 'rock' && computerSelection === 'rock'){
     return youdraw()
     }
     else if (playerSelection === 'rock' && computerSelection === 'scissors'){
-        return youwin()
+        return youwin();
     }
     else if (playerSelection === 'rock' && computerSelection === 'paper'){
        return youlose()
@@ -107,4 +81,69 @@ function playRound() {
       return youdraw()
     }  
 }
+
+//score dom
+const playerscorearea = document.querySelector('#playerscore');
+defaultscore.textContent = playerscore
+playerscorearea.appendChild(defaultscore)
+
+const cpuscorearea = document.querySelector('#computerscore');
+defaultscore1.textContent = computerscore
+cpuscorearea.appendChild(defaultscore1)
+
+
+//UPDATES SCORE
+function score() {
+  defaultscore.textContent = playerscore
+  playerscorearea.appendChild(defaultscore)
+  defaultscore1.textContent = computerscore
+  cpuscorearea.appendChild(defaultscore1)
+}
+
+//AWARDS WINNER AT 5 POINS
+function scorecheck() {
+  if (playerscore === 5){
+    notification.textContent = 'CONGRATULATIONS YOU WIN :)';
+    youwinlose.appendChild(notification)
+    selectionsarea.remove()
+  }
+  else if (computerscore === 5){
+    notification.textContent = 'YOU LOST :(';
+    youwinlose.appendChild(notification)
+    selectionsarea.remove()
+  }
+}
+
+
+///////////////////////////////////////////////////////////
+//starting game
+rock.addEventListener('click', () => {
+      playerSelection = 'rock'
+      notification.remove()
+      playRound()
+      score()
+      scorecheck()
+    })
+paper.addEventListener('click', () => {
+      playerSelection = 'paper'
+      notification.remove()
+      playRound()
+      score()
+      scorecheck()
+    })
+scissors.addEventListener('click', () => {
+      playerSelection = 'scissors'
+      notification.remove()
+      playRound()
+      score()
+      scorecheck()
+    })
+
+
+/////declare playerscore and cpuscore at 0,
+//if player/computer wins add 1 and display it to user.
+//check score, if score is 5 declare a winner.
+
+
+
 
